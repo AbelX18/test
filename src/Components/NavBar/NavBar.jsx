fe5c0dd425efeed6e3723c9d934ba5ad91216648
AbelX18 <abelx8cam@gmail.com>
2021-11-12 19:15:57 -0300

    first commit

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,15 @@
+import "./NavBar.css"
+import ThreePoint from '../../assets/three-point.png'
+import Twitch from '../../assets/icons8-twitch-48.png'
+
+const NavBar= ()=> {
+    return <>
+        <nav className="barra">
+            <ul>
+      
+            </ul>
+        </nav>
+    </>
+};
+
+export default NavBar
